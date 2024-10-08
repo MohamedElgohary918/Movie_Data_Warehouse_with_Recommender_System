b68de55dfa0d5b265adf764ad0bbf6cf17b8681b
Morsi <morsim33@gmail.com>
2024-10-08 21:25:43 +0300

    first db commot with Table Movies

---
+++ Tables/dbo.Movies.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Movies] (
+  [movie_id] [int] NOT NULL,
+  [movie_name] [varchar](255) NULL,
+  [overview] [text] NULL,
+  [duration] [float] NULL,
+  [release_date] [date] NULL,
+  [release_country] [varchar](255) NULL,
+  PRIMARY KEY CLUSTERED ([movie_id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
