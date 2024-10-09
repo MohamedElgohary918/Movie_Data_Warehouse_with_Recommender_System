30466eeeb1dd94f3f9470b34a955a5f96a512944
Morsi <morsim33@gmail.com>
2024-10-10 00:03:25 +0300

    new table movies

---
+++ Tables/dbo.Movies.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Movies] (
+  [movie_id] [int] NOT NULL,
+  [movie_name] [varchar](255) NULL,
+  [release_year] [int] NULL,
+  [imdb_id] [varchar](255) NULL,
+  PRIMARY KEY CLUSTERED ([movie_id])
+)
+ON [PRIMARY]
+GO
