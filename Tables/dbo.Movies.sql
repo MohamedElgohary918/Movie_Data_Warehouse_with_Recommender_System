f05e6dffa9d104871e86256cb96f204a22b60d7d
Morsi <morsim33@gmail.com>
2024-10-11 15:42:06 +0300

    update movies table

--- Tables/dbo.Movies.sql
+++ Tables/dbo.Movies.sql
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 ﻿CREATE TABLE [dbo].[Movies] (
-  [movie_id] [int] IDENTITY,
+  [Movie_id] [int] NOT NULL,
   [movie_name] [varchar](255) NULL,
-  [overview] [text] NULL,
-  [duration] [float] NULL,
-  [release_date] [date] NULL,
-  [release_country] [varchar](255) NULL,
-  PRIMARY KEY CLUSTERED ([movie_id])
-)
-ON [PRIMARY]
-TEXTIMAGE_ON [PRIMARY]
-=======
-﻿CREATE TABLE [dbo].[Movies] (
-  [movie_id] [int] NOT NULL,
-  [movie_name] [varchar](255) NULL,
-  [release_year] [int] NULL,
   [imdb_id] [varchar](255) NULL,
-  PRIMARY KEY CLUSTERED ([movie_id])
+  [year] [int] NULL,
+  PRIMARY KEY CLUSTERED ([Movie_id])
 )
 ON [PRIMARY]
->>>>>>> 30466eeeb1dd94f3f9470b34a955a5f96a512944
 GO
